362f1c6da087bdf45ba924a98e3e9e7c4e5ab787
Krzysiu <kheciek@gmail.com>
2017-10-12 18:30:02 +0200

    Initial commit

---
+++ OSinfo.js
@@ -0,0 +1,22 @@
+var os = require('os');
+var colors = require('colors');
+var timer = require('./timer');
+function getOSinfo() {
+	var type = os.type();
+	if(type === 'Darwin') {
+	type = 'OSX';
+	} else if(type === 'Windows_NT') {
+		type = 'Windows';
+	}
+	var release = os.release();
+	var cpu = os.cpus()[0].model;
+	var uptime = os.uptime();
+	var userInfo = os.userInfo();
+	console.log('System:'.red, type);
+	console.log('Release:'.blue, release);
+	console.log('CPU model:'.green, cpu);
+	console.log('Uptime: ~'.bgRed, timer.print(uptime));
+	console.log('User name:'.bgBlue, userInfo.username);
+	console.log('Home dir:'.bgGreen, userInfo.homedir);
+}
+exports.print = getOSinfo;
---
+++ timer.js
@@ -0,0 +1,9 @@
+function rolex(time) {
+	var hours, minutes, seconds, total = Math.floor(time);
+	hours = Math.floor(total / 3600);
+	total %= 3600;
+	miutes = Math.floor(total / 60);
+	seconds = total % 60;
+	process.stdout.write(hours + ' hrs ' + miutes + ' mins ' + seconds + ' secs ');
+}
+exports.print = rolex;
